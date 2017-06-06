1e16ebdd5d74a59037661fe96b446cdb7ce3d04a
vanezp01 <vanezp01@gmail.com>
2017-06-06 06:52:07 -0700

    add user id and model associations

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,11 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    @item = current_user.items.build
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.build(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
     def destroy
       @item.destroy
       redirect_to root_path
-    end 
+    end
 
   private
 
